2d43fcdfaf3f08ceb5af7691caf4d46b9d6c7bd7
erdongchan <52059081+erdongchanyo@users.noreply.github.com>
2022-05-16 21:07:24 +0800

    Update test.js

--- Surge/Panels/test.js
+++ Surge/Panels/test.js
@@ -22,8 +22,8 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: '标题是改这里么',
       content: '',
-      icon: params.icon,
-      'icon-color': 'params.color',
+      icon: 'play.tv.fill',
+      'icon-color': '#FF2D55',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
