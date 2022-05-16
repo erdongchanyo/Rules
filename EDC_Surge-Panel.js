988ad4c11a9591e2942e70b9a3b039e2146f95a2
erdongchan <52059081+erdongchanyo@users.noreply.github.com>
2022-05-16 15:12:25 +0800

    Update EDC_Surge-Panel.js

--- EDC_Surge-Panel.js
+++ EDC_Surge-Panel.js
@@ -11,7 +11,8 @@ if ($trigger == "button") await httpAPI("/v1/profiles/reload");
 
   $done({
       title:"𝗦𝗨𝗥𝗚𝗘 𝗣𝗥𝗢 𝗘𝗗𝗖_𝗟𝗔𝗭𝗬-𝗖𝗢𝗡𝗙",
-      content:`启动时长: ${startTime}`,
+      content:`测试` +
+	`启动时长: ${startTime}`,
 		icon: params.icon,
 		"icon-color":params.color
     });
