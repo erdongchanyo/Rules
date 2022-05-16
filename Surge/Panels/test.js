a3bed25ddca5030a153890f11790ecdafa968d69
erdongchan <52059081+erdongchanyo@users.noreply.github.com>
2022-05-16 20:40:48 +0800

    Update test.js

--- Surge/Panels/test.js
+++ Surge/Panels/test.js
@@ -1,7 +1,7 @@
 const REQUEST_HEADERS = {
-	'User-Agent':
-	  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
-	'Accept-Language': 'en',
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+    'Accept-Language': 'en',
 }
 
 // 即将登陆
