3da95d002dc958ca6f291ab4b4b6204f970e35be
erdongchan <52059081+erdongchanyo@users.noreply.github.com>
2022-05-16 14:52:06 +0800

    Create EDC_Surge-Panel.js

---
+++ EDC_Surge-Panel.js
@@ -0,0 +1,62 @@
+let params = getParams($argument)
+
+!(async () => {
+/* 时间获取 */
+let traffic = (await httpAPI("/v1/traffic","GET"))
+let dateNow = new Date()
+let dateTime = Math.floor(traffic.startTime*1000)
+let startTime = timeTransform(dateNow,dateTime)
+
+if ($trigger == "button") await httpAPI("/v1/profiles/reload");
+
+  $done({
+      title:"𝗦𝗨𝗥𝗚𝗘 𝗣𝗥𝗢 𝗘𝗗𝗖_𝗟𝗔𝗭𝗬-𝗖𝗢𝗡𝗙",
+      content:`启动时长: ${startTime}`,
+		icon: params.icon,
+		"icon-color":params.color
+    });
+
+})();
+
+function timeTransform(dateNow,dateTime) {
+let dateDiff = dateNow - dateTime;
+let days = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
+let leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
+let hours=Math.floor(leave1/(3600*1000))//计算出小时数
+//计算相差分钟数
+let leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
+let minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
+//计算相差秒数
+let leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+let seconds=Math.round(leave3/1000)
+
+if(days==0){
+
+	if(hours==0){
+	if(minutes==0)return(`${seconds}秒`);
+	return(`${minutes}分${seconds}秒`)
+	}
+	return(`${hours}时${minutes}分${seconds}秒`)
+	}else {
+	return(`${days}天${hours}时${minutes}分`)
+	}
+
+}
+
+
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
+
+function getParams(param) {
+  return Object.fromEntries(
+    $argument
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
+}
